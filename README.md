384ec052a8313307711028d06c1e223db4675b30
Marios Daskalas <25797700+mariosdaskalas@users.noreply.github.com>
2025-04-01 20:20:40 +0300

    Base source code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gopress
+Description: Implement a basic file compression tool that can compress and decompress files using algorithms like Huffman coding or zlib.
+
+Skills: File handling, understanding compression algorithms, bitwise operations.
--- gopress.go
+++ gopress.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+func compress() {
+	// This function will compress the data
+}
+
+func decompress() {
+	// This function will decompress the data
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(` 
+
+	██████   ██████  ██████  ██████  ███████ ███████ ███████ 
+	██       ██    ██ ██   ██ ██   ██ ██      ██      ██      
+	██   ███ ██    ██ ██████  ██████  █████   ███████ ███████ 
+	██    ██ ██    ██ ██      ██   ██ ██           ██      ██ 
+	 ██████   ██████  ██      ██   ██ ███████ ███████ ███████ 
+															  
+															  
+									
+	`)
+	var choice int
+	fmt.Println("[0] : Compress")
+	fmt.Println("[1] : Decompress")
+	fmt.Println()
+
+	fmt.Printf("Select your choice: ")
+	fmt.Scan(&choice)
+
+	if choice == 0 {
+		fmt.Println("Compressing...")
+		compress()
+	} else if choice == 1 {
+		fmt.Println("Decompressing...")
+		decompress()
+	} else {
+		fmt.Println("Invalid choice")
+		return
+	}
+
 }
